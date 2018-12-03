baddffe9684fb2522e0ef31ea7354eef2277a429
Nolan Padgett Jr <nolanjr@ufl.edu>
2018-12-02 23:46:14 -0500

    Added javascript for object, array and while loop

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+//array
+var members = [
+    'Dave Grohl',
+    'Nate Mendel',
+    'Pat Smear',
+    'Taylor Hawkins',
+    'Rami Jaffee',
+    'Chris Shiflett'
+];
+//object
+var tour = {
+    chicago: 'July 29th & 30th, 2018',
+    seattle: 'September 1st, 2018',
+    edmonton: 'September 4th, 2018'
+};
+//While Loop
+var i = 0;
+
+while (i < 5) {
+  console.log('Times running through the code: ' + i);
+
+  i++;
+}
